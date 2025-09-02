f8072001fc990155354fbda41c0b97c83a469f9e
jickDo <pocard0117@naver.com>
2025-09-02 18:05:42 +0900

    fix: 의존성 문제 해결

---
+++ blog-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: "CATCH NPE",
+  description: "CATCH NPE tech blog",
+  author: "jc, 023",
+  siteUrl: "https://catch-npe.netlify.app/",
+  links: {
+    github: "https://github.com/CATCH-NPE"
+  },
+  giscus: {
+    repo: "CATCH-NPE/npe",
+    repoId: "{YOUR_GITHUB_REPOSITORY_ID}",
+    category: "{YOUR_GITHUB_REPOSITORY_CATEGORY}",
+    categoryId: "{YOUR_GITHUB_REPOSITORY_CATEGORY_ID}",
+    mapping: "pathname",
+    strict: "0",
+    reactionsEnabled: "1",
+    inputPosition: "bottom",
+    lang: "en",
+  },
+}
